44331199725834e634c33e0773b7cbae769025ba
jednalig01 <146331424+jednalig01@users.noreply.github.com>
2023-09-28 17:42:11 +0800

    Update streamlit_app.py
    change font

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parent New Healthy Diner')
 
 streamlit.header('Braekfast Menu')
-streamlit.title('sdfsdf')
-streamlit.title('sdfsd')
+streamlit.text('sdfsdf')
+streamlit.text('sdfsd')
 
