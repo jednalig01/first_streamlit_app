87d5eff1fb6db44080cfe2bd617466210f51114f
jednalig01 <146331424+jednalig01@users.noreply.github.com>
2023-09-28 17:46:54 +0800

    Update streamlit_app.py
    add library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parent New Healthy Diner')
 
 streamlit.header('Braekfast Menu')
 streamlit.text('sdfsdf')
 streamlit.text('sdfsd')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
