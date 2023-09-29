70cce1727186cbd95a23a1d5050468fb0ce5ff19
jednalig01 <146331424+jednalig01@users.noreply.github.com>
2023-09-29 10:19:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 streamlit.title('My Parent New Healthy Diner')
 
 streamlit.header('Braekfast Menu')
-streamlit.text('sdfsdf')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('sdfsd')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
